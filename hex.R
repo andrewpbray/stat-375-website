c0ff156eae77963d147331e48078064109727708
Isaac Schmidt <ischmidt20@berkeley.edu>
2021-08-24 20:47:45 -0700

    add R code to make hex

---
+++ hex.R
@@ -0,0 +1,5 @@
+library(hexSticker)
+
+sticker("BEAR_LARGE.png", package = "Stat 375", asp = 1,
+        p_size = 7.5, s_x = 1, s_y = .75, s_width = .6,
+        filename = "output.png", h_fill = "#003262", h_color = "#FDB515", p_color = '#FDB515')
